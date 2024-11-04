1c378632baa3005ad7d9063dcf3d9819f66f65cd
pauuulaaa <heleinapaula@gmail.com>
2024-11-04 19:15:56 +0800

    initial commit

--- classSubject.js
+++ classSubject.js
@@ -3,3 +3,6 @@ let classSchedule = "4:00-5:00 in Monday and Friday"
 let classroom = "Computer Laboratory C"
 let classInstructor = "Mr. Ryan Jay Cuevas"
 let studentName = "Heleina Paula G. Molina"
+
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
